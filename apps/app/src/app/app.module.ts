67291751be1ea7b59c0ce04d1ba9c84e44e503ac
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2017-10-12 15:18:43 -0400

    Create app

---
+++ apps/app/src/app/app.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NxModule} from '@nrwl/nx';
+
+import {AppComponent} from './app.component';
+
+@NgModule({imports: [BrowserModule, NxModule.forRoot()], declarations: [AppComponent], bootstrap: [AppComponent]})
+export class AppModule {}
